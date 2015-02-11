1afd087c4685c7760b66371c0621271c07fb6e8f
Bryan Perez <bperez@mdlive.com>
2015-02-10 23:47:14 -0500

    Used Faraday gem instead of Unirest. Built basic table with info for ten most recent disasters.

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -1,3 +1,4 @@
+require 'faraday'
 require 'json'
 
 class Disaster < ActiveRecord::Base
@@ -9,8 +10,10 @@ class Disaster < ActiveRecord::Base
   private
 
   def self.request_disasters
-    response = Unirest::get "http://api.rwlabs.org/v1/reports"
-    JSON.parse(response.body, symbolize_names:true)
+    @conn = Faraday.new(:url => "http://api.rwlabs.org")
+    response = @conn.get "/v1/reports"
+    # Unirest::get "http://api.rwlabs.org/v1/reports"
+    JSON.parse(response.body, symbolize_names:true)[:data]
   end
 
 end
